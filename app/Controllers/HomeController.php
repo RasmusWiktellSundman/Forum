ec7aa63a7e5a5ffef8be908e745524f10d6da676
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2023-04-05 15:41:16 +0200

    Added support for layouts

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,6 +4,6 @@ namespace App\Controllers;
 class HomeController {
     public function index()
     {
-        renderView('test', ["test" => "Hello123"]);
+        renderView('test', 'base');
     }
 }
--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,16 +1,28 @@
 <?php
 
 /**
- * Renderar en vy
+ * Renderar en vy med standard layout
  *
  * @param string $fileName
+ * @param string|null $layoutName
+ * @param array $variables
  * @return void
  */
-function renderView(string $fileName, array $variables = [])
+function renderView(string $fileName, ?string $layoutName, array $variables = [])
 {
     // Skapar variabler för datan som skickades med till funktionen. Dessa variabler kan sedan användas i vyn.
     foreach ($variables as $key => $value) {
         $$key = $value;
     }
-    require VIEWS_PATH . '/' . $fileName . '.php';
+
+    if($layoutName != null) {
+        // Sparar view i variabel, variabeln används i layouten för att rendera innehållet på rätt plats.
+        ob_start();
+        require VIEWS_PATH . '/' . $fileName . '.php';
+        $content = ob_get_clean();
+
+        require VIEWS_PATH . '/layouts/' . $layoutName . '.php';
+    } else {
+        require VIEWS_PATH . '/' . $fileName . '.php';
+    }
 }
