bb40fdd3c4b5b181c3bcb8104ffd3015189a1e9a
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2023-04-15 18:11:23 +0200

    Cleaned up index.php

--- public/index.php
+++ public/index.php
@@ -4,18 +4,9 @@ use \App\Controllers\HomeController;
 // Läser in composers autoload fil.
 require __DIR__ . '/../vendor/autoload.php';
 
-// Sätter upp globala konstanter
+// Sätter upp globala konstanter och läser in miljövariabler
 define('VIEWS_PATH', __DIR__.'/../views');
 $_ENV = parse_ini_file('../.env');
-
-class Test {
-    public function testing()
-    {
-        $test = "Hello hello";
-        echo "Hello World";
-        renderView('test');
-    }
-}
 
 // Create Router instance
 $router = new \Bramus\Router\Router();
