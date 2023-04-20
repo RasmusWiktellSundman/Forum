5415fd55d4173c96623b986c4e483e647cecc22c
Rasmus Wiktell Sundman <rsundman04@gmail.com>
2023-04-20 12:35:12 +0200

    Renamed 404.css to error.css

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="<?php echo $_ENV['BASE_URL'] ?>/css/404.css">
+<link rel="stylesheet" href="<?php echo $_ENV['BASE_URL'] ?>/css/error.css">
 <h1>404 - Sidan hittades inte</h1>
